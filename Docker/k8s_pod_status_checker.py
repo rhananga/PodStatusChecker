bfb8cbea9cfc61e3a8702a909ba4e911a2be413a
rhananga <roburmhanangala@gmail.com>
2023-08-01 01:22:33 -0500

    Updated the code and the Test-Case

--- Docker/k8s_pod_status_checker.py
+++ Docker/k8s_pod_status_checker.py
@@ -1,38 +1,62 @@
-# k8s_pod_status_checker.py
-
+# kubernetes_service.py
 import os
-import time
 import sys
+import time
 from kubernetes import client, config
 
-def get_pods_in_status(namespace, status):
-    config.load_incluster_config()  # Use in-cluster configuration
+def get_pods_by_status(status):
+    config.load_incluster_config()  # Assuming running in a Kubernetes cluster
 
     v1 = client.CoreV1Api()
+    pods = v1.list_pod_for_all_namespaces(watch=False)
 
-    pods = v1.list_namespaced_pod(namespace)
-    filtered_pods = [pod for pod in pods.items if pod.status.phase == status]
+    pods_in_status = []
+    for pod in pods.items:
+        if pod.status.phase == status:
+            pods_in_status.append(
+                {
+                    #"Namespace": pod.metadata.namespace,
+                    "Name": pod.metadata.name,
+                    "Status": pod.status.phase,
+                }
+            )
+        else:
+            for container_status in pod.status.container_statuses:
+                if container_status.state.terminated and container_status.state.terminated.reason == status:
+                    pods_in_status.append(
+                        {
+                            #"Namespace": pod.metadata.namespace,
+                            "Name": pod.metadata.name,
+                            "Status": status,
+                        }
+                    )
+                elif container_status.state.waiting and container_status.state.waiting.reason == status:
+                    pods_in_status.append(
+                        {
+                            #"Namespace": pod.metadata.namespace,
+                            "Name": pod.metadata.name,
+                            "Status": status,
+                        }
+                    )
 
-    return filtered_pods
+    return pods_in_status
 
-def print_pod_table(pods):
-    print(f"{'POD NAME':40}{'STATUS':15}")
-    print("="*60)
-    for pod in pods:
-        print(f"{pod.metadata.name:40}{pod.status.phase:15}")
-    sys.stdout.flush()
+def print_pods_table(pods):
+    if pods:
+        print(f"\n\nPODS IN {pod_status} STATE :")
+        print(f"{'POD NAME':40}{'STATE':15}")
+        print("="*60)
+        for pod in pods:
+            #print(f"{pod['Namespace']}\t{pod['Name']}\t{pod['Status']}")
+            print(f"{pod['Name']:40}{pod['Status']:15}")
+        sys.stdout.flush()
+    else:
+        print(f"\nNO PODS FOUND IN THE {pod_status} STATE \n")
+        sys.stdout.flush()
 
 if __name__ == "__main__":
-    pod_status = os.environ.get("POD_STATUS", "Running")
-    namespace = "default"  # Update this if you want to check in a specific namespace
-
     while True:
-        try:
-            pods_in_status = get_pods_in_status(namespace, pod_status)
-            print(f"List of Pods in '{pod_status}' status:")
-            print_pod_table(pods_in_status)
-        except Exception as e:
-            print(f"Error occurred: {e}")
-
-        # Wait for 60 seconds before checking again
-        time.sleep(2)
+        pod_status = os.environ.get("POD_STATUS", "Running")
+        pods = get_pods_by_status(pod_status)
+        print_pods_table(pods)
+        time.sleep(2)  # Query every 2 seconds, adjust as needed
